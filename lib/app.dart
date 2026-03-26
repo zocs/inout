84ffda3cbb1aa1e0bac50e9e2413344608ee2446
zocs <zocs@live.com>
2026-03-27 02:01:14 +0800

    chore: bump version to 0.2.5

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,7 @@ import 'pages/home_page.dart';
 import 'pages/setup_wizard_page.dart';
 import 'pages/splash_page.dart';
 
-const appVersion = '0.2.4';
+const appVersion = '0.2.5';
 
 const Map<String, Color> presetColors = {
   'coral': Color(0xFFFF6B5A),
